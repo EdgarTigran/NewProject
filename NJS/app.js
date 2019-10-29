184397df9f643c602d520430fed76847ab96f4c0
EdgarTigran <davtjanne@gmail.com>
2019-10-29 21:25:03 +0400

    New server

--- NJS/app.js
+++ NJS/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
+app.use('/public', express.static('public'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-app.get('/style.css', function (req, res) {
-    res.sendFile(__dirname + '/style.css');
-});
-app.get('/jquery.js', function (req, res) {
-    res.sendFile(__dirname + '/jquery.js');
-})
+// app.get('/style.css', function (req, res) {
+//     res.sendFile(__dirname + '/style.css');
+// });
+// app.get('/jquery.js', function (req, res) {
+//     res.sendFile(__dirname + '/jquery.js');
+// })
 
 
 
